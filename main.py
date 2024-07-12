3650d1abd8fc5e0202ce0882381d886f27db9f56
GleciaGaba <gleciasf@hotmail.com>
2024-07-12 17:08:08 +0200

    firstcommit

---
+++ main.py
@@ -0,0 +1,15 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+#  Nous définissons ici le type des arguments attendus : a: int, b: int, et le type de la valeur de retour -> int
+# Les arguments sont de type int et la sortie de la fonction est également de type int."
+
+
+def add1(a: int, b: int) -> list[int]:
+    return [1, 2, 3]
+
+
+add("2", 1)
+
+# Nous définissons ici le type de retour attendu ->list[int], une liste avec des nombres à l'interieur
